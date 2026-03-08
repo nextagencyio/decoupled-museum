ecbfecf4d0405d13a306ac454a279f151bf9b91d
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import CollectionCard from '../components/CollectionCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Collections | Museum',
--- app/exhibitions/page.tsx
+++ app/exhibitions/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import ExhibitionCard from '../components/ExhibitionCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Exhibitions | Museum',
